a6ea42c3b8f8cd8033861494eae12b4be8ab86ef
panard <panard@localhost.com>
2020-12-21 22:02:36 +0800

    Title: fix mocos cpu usage not change Description: Author:

--- tmux-powerline/segments/cpu.sh
+++ tmux-powerline/segments/cpu.sh
@@ -5,7 +5,8 @@ run_segment() {
 		echo CPU: $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
 		return 0
 	elif shell_is_osx; then
-		cpus_line=$(top -e -l 1 | grep "CPU usage:" | sed 's/CPU usage: //')
+		#cpus_line=$(top -e -l 1 | grep "CPU usage:" | sed 's/CPU usage: //')
+		cpus_line=$(top -l 1 | grep "CPU usage:" | sed 's/CPU usage: //')
 		#cpu_user=$(echo "$cpus_line" | awk '{print $1}'  | sed 's/%//' )
 		#cpu_system=$(echo "$cpus_line" | awk '{print $3}'| sed 's/%//' )
 		cpu_using=$(echo "$cpus_line" | awk '{print $5}'  | sed 's/%//' | awk -F '.' '{print $1}')
