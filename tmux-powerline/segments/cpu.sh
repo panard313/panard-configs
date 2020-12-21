8dcaac6c7fc51d6de147e06a846d3da8e22fbb3f
panard <panard@localhost.com>
2020-12-21 21:54:59 +0800

    Title: fix powerline in maxos Description: Author:

--- tmux-powerline/segments/cpu.sh
+++ tmux-powerline/segments/cpu.sh
@@ -2,26 +2,15 @@
 
 run_segment() {
 	if shell_is_linux; then
-		#cpu_line=$(top -b -n 1 | head | grep "Cpu(s)" )
-		#cpu_user=$(echo "$cpu_line" | grep -Po "(\d+(.\d+)?)(?=%?\s?(us(er)?))")
-		#cpu_system=$(echo "$cpu_line" | grep -Po "(\d+(.\d+)?)(?=%?\s?(sys?))")
-		#cpu_idle=$(echo "$cpu_line" | grep -Po "(\d+(.\d+)?)(?=%?\s?(id(le)?))")
-		#cpu_idle=$(vmstat |awk 'NR==3 {print $15}')
-		cpu_idle=0
-	elif shell_is_osx; then 
-		cpus_line=$(top -e -l 1 | grep "CPU usage:" | sed 's/CPU usage: //')
-		cpu_user=$(echo "$cpus_line" | awk '{print $1}'  | sed 's/%//' )
-		cpu_system=$(echo "$cpus_line" | awk '{print $3}'| sed 's/%//' )
-		cpu_idle=$(echo "$cpus_line" | awk '{print $5}'  | sed 's/%//' )
-	fi
-
-	#if [ -n "$cpu_user" ] && [ -n "$cpu_system" ] && [ -n "$cpu_idle" ]; then
-		#echo "${cpu_user}, ${cpu_system}, ${cpu_idle}" | awk -F', ' '{printf("%5.1f,%5.1f,%5.1f",$1,$2,$3)}'
-	if [ -n "$cpu_idle" ]; then
-		#echo "${cpu_idle}"
-		  echo CPU: $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
+		echo CPU: $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
 		return 0
-	else
-		return 1
+	elif shell_is_osx; then
+		cpus_line=$(top -e -l 1 | grep "CPU usage:" | sed 's/CPU usage: //')
+		#cpu_user=$(echo "$cpus_line" | awk '{print $1}'  | sed 's/%//' )
+		#cpu_system=$(echo "$cpus_line" | awk '{print $3}'| sed 's/%//' )
+		cpu_using=$(echo "$cpus_line" | awk '{print $5}'  | sed 's/%//' | awk -F '.' '{print $1}')
+		echo CPU: $[100- $cpu_using]
+        return 0
 	fi
+    return -1
 }
