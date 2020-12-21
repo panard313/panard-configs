729ecf95bf9759ba855a136ffcdccf4ec422773a
panard <panard@localhost.com>
2020-12-21 22:20:13 +0800

    Title: improve Description: Author:

--- tmux-powerline/segments/cpu.sh
+++ tmux-powerline/segments/cpu.sh
@@ -2,16 +2,14 @@
 
 run_segment() {
 	if shell_is_linux; then
-		echo CPU: $[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
-		return 0
+		cpu_using=$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]
 	elif shell_is_osx; then
 		#cpus_line=$(top -e -l 1 | grep "CPU usage:" | sed 's/CPU usage: //')
 		cpus_line=$(top -l 1 | grep "CPU usage:" | sed 's/CPU usage: //')
 		#cpu_user=$(echo "$cpus_line" | awk '{print $1}'  | sed 's/%//' )
 		#cpu_system=$(echo "$cpus_line" | awk '{print $3}'| sed 's/%//' )
 		cpu_using=$(echo "$cpus_line" | awk '{print $5}'  | sed 's/%//' | awk -F '.' '{print $1}')
-		echo CPU: $[100- $cpu_using]
-        return 0
 	fi
-    return -1
+    printf "CPU:%3d" $[100- $cpu_using]
+    return 0
 }
